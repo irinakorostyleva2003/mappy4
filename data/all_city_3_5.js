023dd593420099969116845dfd600482d6f705c8
irinakorostyleva2003 <irinakorostyleva2003@gmail.com>
2026-05-18 00:51:26 +0300

    Add files via upload

---
+++ data/all_city_3_5.js
@@ -0,0 +1,11 @@
+var json_all_city_3_5 = {
+"type": "FeatureCollection",
+"name": "all_city_3_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name_ru": "Сантьяго-де-Компостела", "1": "1" }, "geometry": { "type": "Point", "coordinates": [ -8.545108800753951, 42.880485830828547 ] } },
+{ "type": "Feature", "properties": { "name_ru": "Ронсесвальес", "1": "2" }, "geometry": { "type": "Point", "coordinates": [ -1.319719003513455, 43.009774005040526 ] } },
+{ "type": "Feature", "properties": { "name_ru": "Ирун", "1": "2" }, "geometry": { "type": "Point", "coordinates": [ -1.790709374472499, 43.33964304998517 ] } },
+{ "type": "Feature", "properties": { "name_ru": "Овьедо", "1": "2" }, "geometry": { "type": "Point", "coordinates": [ -5.843733223155141, 43.362445011734962 ] } }
+]
+}
